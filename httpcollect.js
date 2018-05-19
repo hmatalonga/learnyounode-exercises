b368e35e575bc0e467d15f977569f752032c61f7
Hugo Matalonga <hmatalonga@gmail.com>
2018-05-19 16:14:19 +0100

    feat: add exercises 7-8

--- httpcollect.js
+++ httpcollect.js
@@ -3,9 +3,6 @@
 const http = require('http')
 const bl = require('bl')
 
-// check if argument is provided
-if (process.argv < 3) return
-
 http.get(process.argv[2], response => {
   response.pipe(bl((err, data) => {
     if (err) throw err
--- jugglingasync.js
+++ jugglingasync.js
@@ -3,11 +3,21 @@
 const http = require('http')
 const bl = require('bl')
 
-http.get(process.argv[2], response => {
-  response.pipe(bl((err, data) => {
-    if (err) throw err
-    data = data.toString()
-    console.log(data.length)
-    console.log(data)
-  }))
-}).on('error', console.error)
+const get = url => {
+  return new Promise((resolve, reject) => {
+    http.get(url, response => {
+      response.pipe(bl((err, data) => {
+        if (err) reject(err)
+        resolve(data.toString())
+      }))
+    }).on('error', reject)
+  })
+}
+
+const run = async () => {
+  for (let i = 0; i < 3; i++) {
+    console.log(await get(process.argv[2 + i]))
+  }
+}
+
+run()
