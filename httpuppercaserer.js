c63a3e432222a405557b24d6ca40da9376f9e913
Hugo Matalonga <hmatalonga@gmail.com>
2018-05-23 18:13:11 +0100

    feat: add exercise 12

---
+++ httpuppercaserer.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const http = require('http')
+const map = require('through2-map')
+
+const listener = (request, response) => {
+  if (request.method !== 'POST') {
+    return request.end('Not a POST request\n')
+  }
+  request
+    .pipe(map(chunk => chunk.toString().toUpperCase()))
+    .pipe(response)
+}
+
+http.createServer(listener)
+    .listen(Number(process.argv[2]))
